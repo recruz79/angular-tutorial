2a5833da50fbddd5b94151db2de0233208e9083d
recruz79 <raul.cruz79@gmail.com>
2022-05-06 11:18:59 -0300

    Added Welcome component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,32 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list-component';
 import { ConvertToSpacesPipe } from 'src/shared/convert-to-spaces.pipe';
 import { StarComponent } from 'src/shared/star.component';
+import { ProductsDetailComponent } from './products/products-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductsDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductsDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
